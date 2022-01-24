6226038e8f723b2983b3e84578f218cafec6f8f1
vquine <01010110.v@gmail.com>
2022-01-24 12:07:27 -0500

    add more notes and sectioning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hey. How's it going?        
-    </div>
-  );
+// RUN APP AS CLASS COMPONENT FOR ADDED FUNCTIONALITY
+// ACCESS TO STATE AND LIFECYCLE METHODS
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        THIS IS THE 'APP' COMPONENT
+        <br/>
+        ♥
+      </div>
+    )
+  }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+// THIS PAGE LOADS UP AFTER INDEX.HTML
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=IMPORTS
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=IMPORTSend
 
+
+// DETERMINE WHAT TO RENDER ON THE USER'S PAGE, USING REACT
 ReactDOM.render(
   <React.StrictMode>
+
+    {/* LOAD UP THE APP COMPONENT IMPORTED UP TOP */}
     <App />
+
   </React.StrictMode>,
+
+  // FIND THE ELEMENT WITH THE ID OF ROOT, DELCARED IN INDEX.HTML
+  // LATCH ONTO ELEMENT AND RENDER THERE
   document.getElementById('root')
 );
