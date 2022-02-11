df3618d70843d6bc4e0d59694ae6b8f0e9c4e174
vquine <01010110.v@gmail.com>
2022-02-11 08:25:26 -0500

    artists can now be mapped through with props and displayed to the page

---
+++ src/components/Artists.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import Artist from './Artist';
+import NavigationBar from './NavigationBar';
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+//?  F U N C T I O N A L   C O M P O N E N T
+const Artists = (props) => {
+
+    
+
+    return (
+        <div>
+            <NavigationBar />
+
+            
+            {props.artists.map(a => 
+                <div key={a.id} className='artist'>
+                    
+                    <Link to={`/artgallery/artists/${a.id}`}>                    
+                        <h1>{a.name}</h1>
+                    </Link>
+
+                    <img className="artist-profile-image" src={a.image_url} />
+                    <h1>{a.title}</h1>
+                    <h2>{a.biography}</h2>
+                    <p>{a.skills}</p>
+
+                    <p>{a.location}</p>
+                </div>
+
+                )}
+                 
+        </div>
+    )
+}
+export default Artists;
