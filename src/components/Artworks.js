3e67399ba761dbbe0fa507470b6726d6b68ab49b
vquine <01010110.v@gmail.com>
2022-02-11 08:26:58 -0500

    Artworks component maps through state's handed-in props and maps through each object with an Artwork presentational component

---
+++ src/components/Artworks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Artwork from './Artwork';
+
+//?  F U N C T I O N A L   C O M P O N E N T
+const Artworks = (props) => {
+    return (
+        <div className='artworks'>
+            
+            {props.artworks.map(a => <Artwork artwork={a} key={a.id} />)}
+
+        </div>
+    )
+}
+export default Artworks;
