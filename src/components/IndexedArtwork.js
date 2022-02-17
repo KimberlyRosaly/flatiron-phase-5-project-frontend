c46a648df18986b7754116348e2099826ba1b776
vquine <01010110.v@gmail.com>
2022-02-17 17:17:03 -0500

    beautify | artwork's artist name is clickable and links to artist's show page

--- src/components/IndexedArtwork.js
+++ src/components/IndexedArtwork.js
@@ -1,6 +1,9 @@
 import React from 'react';
-
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { Link } from 'react-router-dom'
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import IndexedArtworkLikeButton from './IndexedArtworkLikeButton'
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 const IndexedArtwork = (props) => {
 
@@ -8,19 +11,17 @@ const IndexedArtwork = (props) => {
 
     return (
         <div className='indexed-artwork'>  
-
             <h2>{props.title}</h2>
-
             
-            <img src={props.image_url} />
-            <br />
+            <img src={props.image_url} /><br />            
 
-            <p>{props.artist.name}</p>
+            <Link to={`/artgallery/artists/${props.artist.id}`}>
+                <p>{props.artist.name}</p>
+            </Link>
 
             <h3>( {props.likes} ) ♥'s</h3>
 
             <IndexedArtworkLikeButton {...props}/>     
-
         </div>
     )
 }
