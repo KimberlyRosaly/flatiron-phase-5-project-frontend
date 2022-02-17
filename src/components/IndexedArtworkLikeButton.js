2f7e83bf1ea3c0b4132638f39e06d1d26ace2b98
vquine <01010110.v@gmail.com>
2022-02-16 21:49:36 -0500

    button can now be disabled with local state changes

--- src/components/IndexedArtworkLikeButton.js
+++ src/components/IndexedArtworkLikeButton.js
@@ -1,19 +1,31 @@
 import React from 'react';
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//^ ALLOW FUNCTIONAL COMPONENT ACCESS TO STATE
+import { useState } from 'react';
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import { connect } from 'react-redux';
-
-//? THE A C T I O N   C R E A T O R  FUNCTION THAT WILL PATCH & DISPATCH ITS ACTION|.TYPE
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//^ THE A C T I O N   C R E A T O R  FUNCTION THAT WILL PATCH & DISPATCH ITS ACTION|.TYPE
 import { likeIndexedArtwork } from '../actions/likeIndexedArtwork';
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
-const IndexedArtworkLikeButton = (props) => {    
-    
+const IndexedArtworkLikeButton = (props) => {
+
+//-------- l O C A L  S T A T E | SET WITH INITIAL VALUE
+    const [state, setState] = useState({toggle: false})
+//------------------------------------------------------
+
     function handleOnClick() {
+        // BUTTON IS CLICKED | ACTION IS DISPATCHED WITH PAYLOAD
         props.likeIndexedArtwork(props.id, props.likes)
+        // BUTTON HAS PROP THAT WILL NOW DISABLE IT
+        setState({toggle: true})
     }
      
     return (
         <div>
-            <button onClick={handleOnClick} className='like-button'>            
+            <button onClick={handleOnClick} className='like-button' disabled={state.toggle}>            
                 ♥
             </button>
         </div>
