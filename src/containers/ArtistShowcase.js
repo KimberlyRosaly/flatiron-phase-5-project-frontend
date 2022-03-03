4cbd037dda33e4d0c2b96b0a3c3e8c1c45cef084
vquine <01010110.v@gmail.com>
2022-03-02 23:45:11 -0500

    Divide up the Showcase component to allow for better styling with flex boxes via a left & right side - give div elements class names to link up on the stylesheet

--- src/containers/ArtistShowcase.js
+++ src/containers/ArtistShowcase.js
@@ -1,12 +1,25 @@
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react';
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import Artworks from '../components/Artworks';
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 const ArtistShowcase = (props) => {
     return (
-        <div className='showcase'>            
-            <h1>{props.artist ? props.artist.name : null}</h1>
-            <img className="artist-profile-image" src={props.artist ? props.artist.image_url : null} />
-            <p>{props.artist ? props.artist.title : null }</p>
+        <div className='showcase'>
+                    <h1>{props.artist ? props.artist.name : null}</h1>
+
+{/*  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  */}
+            <div className='s-container'>
+                <div className='s-left'>
+                    <img src={props.artist ? props.artist.image_url : null} />
+                </div>
+                <div className='s-right'>
+                    <p>{props.artist ? props.artist.title : null }</p>
+                </div>
+            </div>
+{/*  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  */}
+            
 {/*  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  */}
             <Artworks artworks={props.artist ? props.artist.artworks : null} />
 {/*  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  */}            
