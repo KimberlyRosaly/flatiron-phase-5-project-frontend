4ade27682c0def86383b422e073d0a30f638f460
vquine <01010110.v@gmail.com>
2022-03-22 23:42:41 -0400

    set up a show page component to house an individual artwork

---
+++ src/containers/ArtworkShow.js
@@ -0,0 +1,14 @@
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// THIS CONTAINER WILL HOUSE AN INDIVIDUAL ARTWORK'S SHOW PAGE
+const ArtworkShow = (props) => {
+    console.log("HELLO, ARTWORKsHOW PROPS:", props)
+    return (
+        <div>
+            Hello, thar!
+        </div>
+    )
+}
+export default ArtworkShow;
