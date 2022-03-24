5183ef5151278968979740ac7292cd1cfdad413f
vquine <01010110.v@gmail.com>
2022-03-23 22:52:05 -0400

    use route props match property to hang onto the id of the desired artwork from the URL, iterate through props, display it within the component rendering

--- src/containers/ArtworkShow.js
+++ src/containers/ArtworkShow.js
@@ -4,10 +4,18 @@ import React from 'react'
 
 // THIS CONTAINER WILL HOUSE AN INDIVIDUAL ARTWORK'S SHOW PAGE
 const ArtworkShow = (props) => {
-    console.log("HELLO, ARTWORKsHOW PROPS:", props)
+    // console.log("HELLO, ARTWORKsHOW PROPS:", props)
+    let theArtworkID = props.match.params.id
+    // console.log("PARAMS:", theArtworkID)
+    let theArtwork = props.artworks.find((a) => a.id == theArtworkID)
+    // console.log("ARTWORK FOUND???:", theArtwork)
+
     return (
         <div>
-            Hello, thar!
+            {theArtwork.title}
+            {/* ▼ IMAGE LIVES HERE ▼ */}
+            <div style={{ backgroundImage: `url(${theArtwork.image_url})` }} className='artwork-show-image' />
+            BY : {theArtwork.artist.name}
         </div>
     )
 }
