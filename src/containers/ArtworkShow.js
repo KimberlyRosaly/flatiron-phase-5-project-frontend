62383ad9353dae12402e710809a6789ff24c47da
vquine <01010110.v@gmail.com>
2022-03-23 23:52:01 -0400

    artwork's show page has a navigation bar imported and on display for user's navigatory delight!

--- src/containers/ArtworkShow.js
+++ src/containers/ArtworkShow.js
@@ -1,21 +1,24 @@
 //^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
 //^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import NavigationBar from '../components/NavigationBar'
+//^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-// THIS CONTAINER WILL HOUSE AN INDIVIDUAL ARTWORK'S SHOW PAGE
+// THIS  C O N T A I N E R  WILL HOUSE AN INDIVIDUAL ARTWORK'S SHOW PAGE
 const ArtworkShow = (props) => {
     
     let theArtworkID = props.match.params.id
     const theArtwork = props.artworks.find((a) => a.id == theArtworkID)
 
-
-
     return (
         <div>
-            {theArtwork ? theArtwork.title : null}
-            {/* ▼ IMAGE LIVES HERE ▼ */}
-            <div style={{ backgroundImage: `url(${theArtwork ? theArtwork.image_url : null})` }} className='artwork-show-image' />
-            BY : {theArtwork ? theArtwork.artist.name : null}
+            <NavigationBar />
+            <div>
+                {theArtwork ? theArtwork.title : null}
+                {/* ▼ IMAGE LIVES HERE ▼ */}
+                <div style={{ backgroundImage: `url(${theArtwork ? theArtwork.image_url : null})` }} className='artwork-show-image' />
+                BY : {theArtwork ? theArtwork.artist.name : null}
+            </div>
         </div>
     )
 }
