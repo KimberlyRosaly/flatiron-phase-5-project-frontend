49138c21e96e40d580e27fa857bb4b27d64e8516
vquine <01010110.v@gmail.com>
2022-03-30 00:06:09 -0400

    make the name block have a background image of the artist from their profiles

--- src/components/ArtistHeader.js
+++ src/components/ArtistHeader.js
@@ -3,8 +3,18 @@ import React from 'react';
 //^ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 const ArtistHeader = (props) => {
+
+    const backgroundStyle = {
+        backgroundImage: `linear-gradient(90deg, rgba(2,0,36,1) 40%, rgba(14,14,33,0) 100%)
+        ,
+        url(${props.artist ? props.artist.image_url : null})
+        `,    
+        backgroundSize: "150%",
+        backgroundRepeat: 'no-repeat'
+    }
+    
     return(
-        <div className='artist-header'>
+        <div className='artist-header' style={backgroundStyle}>
             <h1 style={{fontSize: '75px'}}>{props.artist ? props.artist.name : null}</h1>
             <p>{props.artist ? props.artist.title : null}</p>
         </div>
